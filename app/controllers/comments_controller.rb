271c44331acc8b2bd7d6e25f792a1e2e7ce8ecc9
Murugan <murugu2012@gmail.com>
2016-11-08 20:22:55 +0530

    Generate comments resource with starter:resource

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,63 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+
+    render("comments/index.html.erb")
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+
+    render("comments/show.html.erb")
+  end
+
+  def new
+    @comment = Comment.new
+
+    render("comments/new.html.erb")
+  end
+
+  def create
+    @comment = Comment.new
+
+    @comment.user_id = params[:user_id]
+    @comment.photo_id = params[:photo_id]
+
+    save_status = @comment.save
+
+    if save_status == true
+      redirect_to(:back, :notice => "Comment created successfully.")
+    else
+      render("comments/new.html.erb")
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+
+    render("comments/edit.html.erb")
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    @comment.user_id = params[:user_id]
+    @comment.photo_id = params[:photo_id]
+
+    save_status = @comment.save
+
+    if save_status == true
+      redirect_to(:back, :notice => "Comment updated successfully.")
+    else
+      render("comments/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    @comment.destroy
+
+    redirect_to(:back, :notice => "Comment deleted.")
+  end
+end
